9394f3f995d7d34d5e954db2f0d21708b1e13f44
LouiMinister <LouiMinister@gmail.com>
2020-07-11 01:40:10 +0900

    Finish testing excel_writer module

--- public/index.js
+++ public/index.js
@@ -2,7 +2,7 @@ import {multiCrawl} from './YonginCrawler.js';
 import {jsonToExcel} from './excel_writer'
 
 (async ()=>{
-    const lectureList = await multiCrawl(1, 4);
+    const lectureList = await multiCrawl(1, 20);
     const header =["번호","분류","강좌명","기관","URL","신청기간","교육기간"];
     const colWidth = [
           { wch : 5 }   // A열
@@ -15,5 +15,5 @@ import {jsonToExcel} from './excel_writer'
     ];
     const sheetName = "용인";
     const filePath = "doc_files/dramatis_personae.xlsx";
-    expect(jsonToExcel({jsonAry: lectureList, colWidth: colWidth, filePath: filePath, sheetName :sheetName}));
+    jsonToExcel({jsonAry: lectureList, colWidth: colWidth, filePath: filePath, sheetName :sheetName});
 })();
